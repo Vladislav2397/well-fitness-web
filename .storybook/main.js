b22051b2a207f9429ad9ec113f7ba26cd6f3ef57
Vl@dislav <vt@burno.tech>
2022-12-01 06:41:55 +0300

    Fix storybook preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,37 @@
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/vue",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
+    stories: [
+        '../src/shared/**/*.stories.mdx',
+        '../src/shared/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/preset-scss',
+    ],
+    webpackFinal: config => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname, '../src')
+        }
+
+        config.module.rules.push({
+            test: /\.pug$/,
+            use: [
+                {
+                    loader: 'pug-bem-plain-loader',
+                    options: {
+                        b: true,
+                    }
+                },
+            ]
+        })
+        return config
+    },
+    framework: '@storybook/vue',
+    core: {
+        builder: '@storybook/builder-webpack5',
+    },
 }
--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,19 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
+    '@vue/cli-plugin-babel/preset',
+  ],
+  plugins: [
+    [
+        'module-resolver',
+        {
+            extensions: ['.js', '.jsx', '.ts', '.tsx', '.svg'],
+            alias: {
+                // This needs to be mirrored in
+                // tsconfig.json and jsconfig.json
+                '@': './src',
+                '~p': './public',
+            },
+        },
+    ],
   ]
 }
